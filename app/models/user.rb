a65c1840ec7be27847dffa1c1a54f2c5293af95f
Venjaminnn <victoriagalchenko@gmail.com>
2023-01-20 16:58:15 +0300

    Added user model and users migration

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+
+  validates :first_name, :last_name, length: { maximum: 100 }, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }, presence: true, length: { maximum: 150 }
+end
