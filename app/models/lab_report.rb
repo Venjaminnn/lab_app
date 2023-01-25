c02616f8751cacaca892e06536a75c1a7a04b2c5
Venjaminnn <victoriagalchenko@gmail.com>
2023-01-25 13:30:53 +0300

    Added model user and model lab_report

--- app/models/lab_report.rb
+++ app/models/lab_report.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class LabReports < ApplicationRecord
+class LabReport < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :description, length: { maximum: 500 }
-  validates :grade, inclusion: { in: %w[A B C D E FX F] }
+  validates :grade, inclusion: { in: %w[A B C D E FX F] }, allow_nil: true
 end
