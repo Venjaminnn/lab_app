e2fd906616247be891de5133febde29fdf9a6225
Venjaminnn <victoriagalchenko@gmail.com>
2023-01-25 13:42:36 +0300

    Added deleting lab reports, route for it, and added stylesheets for pages

--- app/controllers/lab_reports_controller.rb
+++ app/controllers/lab_reports_controller.rb
@@ -6,6 +6,10 @@ class LabReportsController < ApplicationController
   def new
   end
 
+  def show
+    @lab_report = @lab_reports.find(params[:id])
+  end
+
   def index
     @lab_reports = LabReport.all.includes(:user)
   end
@@ -19,7 +23,7 @@ class LabReportsController < ApplicationController
       render :new, flash: { alert: 'Something went wrong' }
     end
   end
-
+  
   def update
     lab_report = @lab_reports.find_by(id: params[:id])
 
@@ -31,7 +35,19 @@ class LabReportsController < ApplicationController
       render :new, flash: { alert: 'Something went wrong' }
     end
   end
-  
+
+  def destroy
+    lab_report = LabReport.find_by(id: params[:id])
+
+    if lab_report
+      lab_report.destroy
+
+      redirect_to lab_reports_url, flash: { success: 'Report was deleted' }
+    else
+      render :new, flash: { alert: 'Something went wrong' }
+    end
+  end
+
   private
 
   def set_collection
@@ -42,7 +58,7 @@ class LabReportsController < ApplicationController
     params.permit(:grade)
   end
 
-    def lab_report_params
+  def lab_report_params
     params.permit(:title, :description, :user_id).permit!
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-    resources :lab_reports, only: %i[new create index update show]
+    resources :lab_reports, only: %i[new create index update show destroy]
 end
